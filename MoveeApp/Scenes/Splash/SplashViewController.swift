742c775f5f20e04a7ba216525456816b744175a3
Can Yoldas <can.yoldas0@outlook.com>
2022-04-14 01:27:00 +0300

    first commit

--- MoveeApp/Scenes/Splash/SplashViewController.swift
+++ MoveeApp/Scenes/Splash/SplashViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SplashViewController.swift
 //  MoveeApp
 //
 //  Created by Can Yoldaş on 14.04.2022.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SplashViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
 }
-
